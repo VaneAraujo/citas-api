8e9819e64ff81c953c7d472c282399b09b879f3d
VaneAraujo <113569291+VaneAraujo@users.noreply.github.com>
2022-12-07 13:10:15 -0500

    Servicios de médico

--- src/main/java/com/example/citasapi/services/MedicoService.java
+++ src/main/java/com/example/citasapi/services/MedicoService.java
@@ -1,8 +1,33 @@
 package com.example.citasapi.services;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MedicoService {
-    
+    @Autowired
+    MedicoRepository repositorio;
+    @Autowired
+    PersonaRepository repositorioPersona;
+
+    public MedicoModelo guardarMedico(MedicoModelo medico){
+        repositorioPersona.save(medico.getPersona());
+        return repositorio.save(medico);
+    }
+
+    public ArrayList<MedicoModelo> consultarMedicos(){
+        return(ArrayList<MedicoModelo>) repositorio.findAll();
+    }
+
+    public boolean eliminarMedico(Long id){
+        if(repositorio.existById(id)){
+            repositorio.deleteById(id)
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
